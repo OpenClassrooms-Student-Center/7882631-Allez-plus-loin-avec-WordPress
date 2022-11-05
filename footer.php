e6e4ad578da3747ba89c3d625fe5b515b846593d
Quentin LAURENT <quentin.laurent@timbreos.com>
2022-11-05 16:37:29 +0100

    Widget Image Titre

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+			<footer id="site-footer" class="header-footer-group">
+                ©BricoTips depuis 2022
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
--- functions.php
+++ functions.php
@@ -7,5 +7,22 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du css/theme.css pour nos personnalisations
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-
+    // Chargement du /css/widgets/image-titre-widget.css pour notre widget image titre
+    wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
 }
+
+/* CHARGEMENT DES WIDGETS */
+
+
+// On créer une class Widget Image_Titre_Widget dans un fichier à part pour pas surcharger le functions.php
+require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+
+
+
+function register_widgets()
+{
+    //On enregistre le widget avec la class Image_Titre_Widget
+    register_widget('Image_Titre_Widget');
+}
+//On demander à wordpress de charger des widget selon la fonction register_widgets()
+add_action('widgets_init', 'register_widgets');

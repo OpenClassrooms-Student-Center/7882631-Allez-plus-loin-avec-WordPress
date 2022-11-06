8044a03df90972f36dada8f0dc53b0445da312bd
Quentin LAURENT <quentin.laurent@timbreos.com>
2022-11-06 15:26:29 +0100

    Créer un template de page

---
+++ templates/template-a-propos.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: template à propos
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+    Nous sommes dans la page à propos.
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
